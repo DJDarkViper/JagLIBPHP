f1e09784eee31bd6f551fec5ecabafbde6a35176
Kyle Harrison <kyle@navigatormm.com>
2011-12-12 10:52:45 -0800

    added new flatdb class, this is a database driver designed to handle all things flatfile database. Plan is to start with PHP Serialized data, move to XML then JSON.
    Signed-off-by: Kyle Harrison <kyle@navigatormm.com>

---
+++ classes/flatdb.class.php
@@ -0,0 +1,15 @@
+<?php
+class FlatDB {
+	
+	public $File = null;
+	
+	
+	/**
+	 * A class to manage Flatfile databases (aka: No-DB Support). Utilizes PHP Serialized Data
+	 * 
+	 */
+	public function __construct($filename) {
+		
+	}
+	
+}
